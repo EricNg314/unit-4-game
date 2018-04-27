c4907b6e4ca585d45dead2e25fdcd43f141e3be9
Okens <Ng.Eric314@gmail.com>
2018-04-26 22:35:43 -0700

    Updating with placeholder image of status. Adding pseudo code for game expectations in javascript.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+    //========== Initialization Part 1 ===================================================//
+
+    //Declare guesses and values to be stored.
+    //Values are "scoreWins", "scoreLoss" "desiredKibbles","currentKibbles", "kibblesBtn1", 
+    //"kibblesBtn2","kibblesBtn3", "kibblesBtn4"
+
+    
+    //========= Initialization Part 2 ====================================================//
+
+    //gameReset()
+    ////Get random desired kibbles
+    ////Set current kibbles = 0
+    ////Assign values to each button.
+    ////Display hungry cat.
+    ////Display values.
+    
+
+    //========== Clicking Event ===================================================//
+
+    //Display hungry cat, for new round as image.
+    //Check for user clicking.
+    //Check if user selected a class button choice, then pull initiated value.
+    //Add initiated value to current kibbles.
+
+
+    //========== Clicking Event (Check if win/loss)===================================================//
+
+    //Check if current kibbles = desired kibbles.
+    //==TRUE==//
+    //scoreWin + 1
+    //Sound of happy kitty.
+    //Output a you win message.
+    ////gameReset()
+
+    //==ELSE IF current > desired==//
+    //scoreLoss + 1
+    //Sound of hissing kitty.
+    //Output an angry kitty.
+    //gameReset()
+
+    //==ELSE==//
+    //Sound of annoying/hungry kitty.
+
+
+    //=============================================================//
+
+
+
+
+
+
+});

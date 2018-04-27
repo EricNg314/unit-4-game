047d0dd029481f2c9984defb2732f69a8ecf6c32
Okens <Ng.Eric314@gmail.com>
2018-04-27 01:16:33 -0700

    Creating logic to correct end goal is odd and buttons are all even.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -47,25 +47,26 @@
 //=============================================================//
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     //Initialization Part 1
     var scoreWins = 0;
     var scoreLoss = 0;
     var goalKibbles = 0;
     var currKibbles = 0;
-    var kibblesBtn1 = 0;
-    var kibblesBtn2 = 0;
-    var kibblesBtn3 = 0;
-    var kibblesBtn4 = 0;
+    var kibblesBtnArray = [];
+    var kibblesBtn1 = $("#button-1");
+    var kibblesBtn2 = $("#button-2");
+    var kibblesBtn3 = $("#button-3");
+    var kibblesBtn4 = $("#button-4");
 
     //Initialization Part 2
+    gameReset();
 
     //Clicking Event
     $("body").on("click", ".btn-choice", function () {
 
         console.log(this.id);
-
 
 
 
@@ -86,12 +87,48 @@ $(document).ready(function() {
     };
 
     function getKibbleBtns() {
-        var isOdd = false;
-        goalKibbles = Math.floor((Math.random() * 100) + 19)
-        console.log(goalKibbles);
-        isOdd = goalKibbles % 2 == 0;
-        console.log(isOdd);
+        var isGoalOdd = false;
+        var isKibbleBtnsOdd = false;
+        var isSmallestValue = 9999; //Setting a value far above 0;
+        goalKibbles = Math.floor(Math.random() * 100) + 19;
+        // console.log(goalKibbles);
 
+        // console.log(isOdd);
+
+        //TODO add random numbers for buttons.
+
+        for (var i = 0; i <= 3; i++) {
+            kibblesBtnArray.push(Math.floor(Math.random() * 12) + 1);
+        }
+        kibblesBtnArray = [2, 4, 6, 8];
+        console.log(kibblesBtnArray);
+
+
+        //if goal is odd, check if all buttons are even. if TRUE then + 1 to smallest button.      
+        isGoalOdd = (goalKibbles % 2 !== 0); //give isGoalOdd = true if goalKibble is odd.
+        isSmallestValue = Math.min(...kibblesBtnArray);  
+        if (isGoalOdd === true) {
+            for (var i = 0; i <= 3; i++) {
+                isKibbleBtnsOdd = kibblesBtnArray[i] % 2 !== 0; //give isKibbleBtnsOdd = true if goalKibble is odd.
+                if (isKibbleBtnsOdd === true) {
+                    break;
+                }
+            }
+            if (isKibbleBtnsOdd === false) {
+                for (var i = 0; i <= 3; i++) {
+                    if (isSmallestValue === kibblesBtnArray[i]) {
+                        kibblesBtnArray[i] += 1;
+                        break;
+                    }
+                }
+            }
+        }
+        // console.log(kibblesBtnArray);
+
+
+        //TODO assign array variables to each button.
     };
+
+
 
 });

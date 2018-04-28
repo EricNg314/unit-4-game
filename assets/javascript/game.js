76228b2a170f9b930160ce8b9d335901c0407ed3
Okens <Ng.Eric314@gmail.com>
2018-04-28 13:38:01 -0700

    Updating with display output, images, user's number counter and win/loss check.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -49,47 +49,62 @@
 
 $(document).ready(function () {
 
-    //Initialization Part 1
+    //========== Initialization Part 1 ===================================================//
     var scoreWins = 0;
     var scoreLoss = 0;
     var goalKibbles = 0;
     var currKibbles = 0;
     var kibblesBtnArray = [];
-    var kibblesBtn1 = $("#button-1");
-    var kibblesBtn2 = $("#button-2");
-    var kibblesBtn3 = $("#button-3");
-    var kibblesBtn4 = $("#button-4");
+    // var kibblesBtn1 = $("#button-1");
+    // var kibblesBtn2 = $("#button-2");
+    // var kibblesBtn3 = $("#button-3");
+    // var kibblesBtn4 = $("#button-4");
+    var catStatusImgArray = ["assets/images/cat_hungry.jpg", "assets/images/cat_unhappy.jpg", "assets/images/cat_happy.jpg"];
+    var catStatusImg = catStatusImgArray[0]; //initializing.
 
-    //Initialization Part 2
+    //========= Initialization Part 2 ====================================================//
     gameReset();
+    outputDisplay()
 
-    //Clicking Event
+    //========== Clicking Event ===================================================//
     $("body").on("click", ".btn-choice", function () {
 
         console.log(this.id);
-        // update user pick() += 
+        currKibbles += parseInt(this.value);
+        catStatusImg = catStatusImgArray[0];
 
+        //========== Clicking Event (Check if win/loss)===================================================//
+        if (currKibbles === goalKibbles) {
+            scoreWins++;
+            catStatusImg = catStatusImgArray[2];
+            gameReset();
+        } else if (currKibbles > goalKibbles) {
+            scoreLoss++;
+            catStatusImg = catStatusImgArray[1];
+            gameReset();
+        }
 
-
-
-
+        outputDisplay();
     });
 
 
 
     function gameReset() {
-        var goalKibbles = 0;
-        var currKibbles = 0;
+        goalKibbles = 0;
+        currKibbles = 0;
 
-        goalKibbles = Math.floor(Math.random() * 100) + 19;
         // console.log(goalKibbles);
         getKibbleBtns();
+        getGoalKibbles();
+        console.log("goal kibbles: " + goalKibbles);
+
     };
 
     function getKibbleBtns() {
         var isGoalOdd = false;
         var isKibbleBtnsOdd = false;
         var isSmallestValue = 9999; //Setting a value far above 0;
+        kibblesBtnArray.length = 0; //resetting for new games.
 
         for (var i = 0; i <= 3; i++) {
             kibblesBtnArray.push(Math.floor(Math.random() * 12) + 1);
@@ -99,7 +114,7 @@ $(document).ready(function () {
 
         //if goal is odd, check if all buttons are even. if TRUE then + 1 to smallest button.      
         isGoalOdd = (goalKibbles % 2 !== 0); //give isGoalOdd = true if goalKibble is odd.
-        isSmallestValue = Math.min(...kibblesBtnArray);  
+        isSmallestValue = Math.min(...kibblesBtnArray);
         if (isGoalOdd === true) {
             for (var i = 0; i <= 3; i++) {
                 isKibbleBtnsOdd = kibblesBtnArray[i] % 2 !== 0; //give isKibbleBtnsOdd = true if goalKibble is odd.
@@ -107,7 +122,7 @@ $(document).ready(function () {
                     break;
                 }
             }
-            if (isKibbleBtnsOdd === false) {
+            if (isKibbleBtnsOdd === false) { //if there's no odd, then goal is unsolvable. Make the lowest even value odd.
                 for (var i = 0; i <= 3; i++) {
                     if (isSmallestValue === kibblesBtnArray[i]) {
                         kibblesBtnArray[i] += 1;
@@ -119,12 +134,30 @@ $(document).ready(function () {
         // console.log(kibblesBtnArray);
 
         //TODO assign array variables to each button.
-        kibblesBtn1.val(kibblesBtnArray[0]);
-        kibblesBtn2.val(kibblesBtnArray[1]);
-        kibblesBtn3.val(kibblesBtnArray[2]);
-        kibblesBtn4.val(kibblesBtnArray[3]);
+        $("#button-1").val(kibblesBtnArray[0]);
+        $("#button-2").val(kibblesBtnArray[1]);
+        $("#button-3").val(kibblesBtnArray[2]);
+        $("#button-4").val(kibblesBtnArray[3]);
     };
 
 
+    function getGoalKibbles() {
+        do {
+            goalKibbles = 0;
+            for (var i = 0; i < kibblesBtnArray.length; i++) {
+                goalKibbles += kibblesBtnArray[i] * Math.floor(Math.random() * 5);
+            }
+        } while (goalKibbles <= 19 && goalKibbles >= 120);
+    }
+
+
+    function outputDisplay() {
+        var htmlGoalKibble = goalKibbles;
+        var htmlCurrKibble = currKibbles;
+        var htmlCatStatusImg = $("#imgWinLoss").attr('src', catStatusImg)
+
+        document.querySelector("#computer-pick").innerHTML = htmlGoalKibble;
+        document.querySelector("#user-pick").innerHTML = htmlCurrKibble;
+    }
 
 });
